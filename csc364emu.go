b1c823be9d085b8e2eed551c528e427fe416f3b8
John Hawkins <littlehawk93@users.noreply.github.com>
2018-03-16 08:32:52 -0500

    created initial terminal user interface using gocui

--- csc364emu.go
+++ csc364emu.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/jroimartin/gocui"
 )
 
 const (
@@ -45,6 +47,9 @@ type emulator struct {
 	TickTime  uint64
 }
 
+var emu *emulator
+var gui *gocui.Gui
+
 func main() {
 
 	tickTime := flag.Uint64("t", 1000, "Sleep time between emulator clock cycles")
@@ -59,25 +64,177 @@ func main() {
 		panic(fmt.Sprintf("Invalid ROM file provided: %s", err.Error()))
 	}
 
-	emu := newEmulator()
+	emu = newEmulator()
+
+	gui = initGui()
+
+	defer gui.Close()
 
 	if tickTime != nil && *tickTime >= 0 {
 		emu.TickTime = *tickTime
 	}
+
+	if err := gui.MainLoop(); err != nil && err != gocui.ErrQuit {
+		panic(err)
+	}
+}
+
+func initGui() *gocui.Gui {
+
+	tmp, err := gocui.NewGui(gocui.OutputNormal)
+
+	if err != nil {
+		panic(err)
+	}
+
+	tmp.Mouse = false
+
+	tmp.SetManagerFunc(layout)
+
+	if err = tmp.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+
+		tmp.Close()
+		panic(err)
+	}
+
+	return tmp
+}
+
+func layout(g *gocui.Gui) error {
+
+	g.BgColor = gocui.ColorBlack
+	g.FgColor = gocui.ColorWhite
+
+	titleView, err := g.SetView("title", 2, 0, 30, 2)
+
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+
+	titleView.BgColor = gocui.ColorBlack
+	titleView.FgColor = gocui.ColorWhite
+	titleView.Editable = false
+	titleView.Frame = false
+
+	titleView.Clear()
+
+	fmt.Fprint(titleView, " LA Tech CSC 364 Emulator ")
+
+	clockView, err := g.SetView("clock", 32, 0, 52, 2)
+
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+
+	clockView.BgColor = gocui.ColorBlack
+	clockView.FgColor = gocui.ColorWhite
+	clockView.Editable = false
+	clockView.Frame = false
+
+	clockView.Clear()
+
+	fmt.Fprintf(clockView, "Clock: %12d", emu.Clock)
+
+	screenView, err := g.SetView("screen", 2, 3, 19, 12)
+
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+
+	screenView.BgColor = gocui.ColorBlack
+	screenView.FgColor = gocui.ColorGreen
+	screenView.Editable = false
+	screenView.Frame = true
+	screenView.Title = "Screen"
+
+	screenView.Clear()
+
+	registersView, err := g.SetView("registers", 21, 3, 52, 12)
+
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+
+	registersView.BgColor = gocui.ColorBlack
+	registersView.FgColor = gocui.ColorWhite
+	registersView.Editable = false
+	registersView.Frame = true
+	registersView.Title = "Registers"
+
+	registersView.Clear()
+
+	renderEmulator(g)
+
+	return nil
+}
+
+func renderEmulator(g *gocui.Gui) error {
+
+	registersView, err := g.View("registers")
+
+	if err != nil {
+		return err
+	}
+
+	registersView.Clear()
+
+	for i := 0; i < len(emu.Registers)/2; i++ {
+
+		fmt.Fprintf(registersView, " %s  | %04X | %s | %04X \n", getRegisterTitle(i), emu.Registers[i], getRegisterTitle(i+8), emu.Registers[i+8])
+	}
+
+	screenView, err := g.View("screen")
+
+	screenView.Clear()
+
+	for x := 0x80; x != 0; x >>= 1 {
+
+		for i := 0; i < len(emu.Screen); i++ {
+
+			if emu.Screen[i]&byte(x) == 0 {
+				fmt.Fprintf(screenView, "%c", ' ')
+			} else {
+				fmt.Fprintf(screenView, "%c", '\u2588')
+			}
+		}
+
+		fmt.Fprintf(screenView, "%c", '\n')
+	}
+
+	return nil
+}
+
+func getRegisterTitle(index int) string {
+
+	if index == regIn {
+		return "INPUT"
+	} else if index == regOut1 {
+		return "OUT 1"
+	} else if index == regOut2 {
+		return "OUT 2"
+	} else if index == regPC {
+		return "PROGC"
+	} else {
+		return fmt.Sprintf("REG %1X", index)
+	}
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
 
 func newEmulator() *emulator {
 
-	var emu emulator
+	var tmp emulator
 
-	emu.Clock = 0
-	emu.TickTime = 1000
-	emu.Screen = make([]byte, hwScreenWidth)
-	emu.Registers = make([]uint16, hwRegCount)
-	emu.RAM = make([]byte, hwRAMSize)
-	emu.ROM = make([]uint16, hwROMSize)
+	tmp.Clock = 0
+	tmp.TickTime = 1000
+	tmp.Screen = make([]byte, hwScreenWidth)
+	tmp.Registers = make([]uint16, hwRegCount)
+	tmp.RAM = make([]byte, hwRAMSize)
+	tmp.ROM = make([]uint16, hwROMSize)
 
-	return &emu
+	return &tmp
 }
 
 func (me *emulator) emulate() {
@@ -98,6 +255,8 @@ func (me *emulator) emulate() {
 		}
 
 		me.Clock++
+
+		gui.Update(renderEmulator)
 
 		if me.TickTime > 0 {
 			time.Sleep(time.Duration(me.TickTime) * time.Millisecond)
